dddb23517ca33d2bda01a2f3152dcc03efa35cdf
hajle <hajlesellasje@gmail.com>
2023-11-10 13:24:32 +0700

    a

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  en: 'English',
+  pl: 'Polski',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.about': 'About Us',
+    'nav.countries': 'Countries',
+    'nav.trips': 'Trips types',
+    'nav.contact': 'Contact',
+  },
+  pl: {
+    'nav.about': 'O nas',
+    'nav.countries': 'Kraje',
+    'nav.trips': 'Rodzaje wycieczek',
+    'nav.contact': 'Kontakt',
+  },
+} as const;
