b24eec0d6fa1a403feb1fea68b05443203211e36
hajle <hajlesellasje@gmail.com>
2023-11-13 22:03:40 +0700

    a

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,43 +1,13 @@
+import { en, pl } from "../data/settings.json";
+
 export const languages = {
-  en: 'English',
-  pl: 'Polski',
+  en: "English",
+  pl: "Polski",
 };
 
-export const defaultLang = 'en';
+export const defaultLang = "en";
 
 export const ui = {
-  en: {
-    'nav.about': 'About Us',
-    'nav.countries': 'Countries',
-    'nav.trips': 'Types of Trips',
-    'nav.contact': 'Contact',
-    'slug.about': 'about-us',
-    'slug.countries': 'countries',
-    'slug.trips': 'types-of-trips',
-    'slug.contact': 'contact',
-    'form.name': 'Name',
-    'form.email': 'Email address',
-    'form.message': 'Message',
-    'form.button': 'Send message',
-    'form.sending': 'Sending...',
-    'form.reply': 'Thank you for contacting us! We will respond to your message soon.',
-    'form.error': 'Error sending message.',
-  },
-  pl: {
-    'nav.about': 'O nas',
-    'nav.countries': 'Kraje',
-    'nav.trips': 'Rodzaje wycieczek',
-    'nav.contact': 'Kontakt',
-    'slug.about': 'o-nas',
-    'slug.countries': 'kraje',
-    'slug.trips': 'rodzaje-wycieczek',
-    'slug.contact': 'kontakt',
-    'form.name': 'Imię',
-    'form.email': 'Adres email',
-    'form.message': 'Wiadomość',
-    'form.button': 'Wyślij wiadomość',
-    'form.sending': 'Wysyłam...',
-    'form.reply': 'Dziękujemy za kontakt! Wkrótce odpowiemy na Twoją wiadomość.',
-    'form.error': 'Błąd podczas wysyłania wiadomości.',
-  },
+  en,
+  pl,
 } as const;
