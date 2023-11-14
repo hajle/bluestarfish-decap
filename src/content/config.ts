063c6c52c32688370223b30cc684b0bd72e8d317
hajle <hajlesellasje@gmail.com>
2023-11-14 15:41:33 +0700

    a

--- src/content/config.ts
+++ src/content/config.ts
@@ -24,6 +24,13 @@ const pageCollection = defineCollection({
       countriesWidget: z.boolean().default(false),
       skillsWidget: z.boolean().default(false),
       tripsWidget: z.boolean().default(false),
+      tripsWidgetPictures: z.object({
+        image0: image().optional(),
+        image1: image().optional(),
+        image2: image().optional(),
+        image3: image().optional(),
+        image4: image().optional(),
+      }),
       contactForm: z.boolean().default(false),
       chatWidget: z.boolean().default(false),
     }),
