c5f22c70d6e76357bd2f1007ed0f41d4bb90effb
hajle <hajlesellasje@gmail.com>
2024-01-03 13:52:24 +0700

    aa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     '/': '/en'
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap()]
 });
